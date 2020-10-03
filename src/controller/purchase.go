6bbd1d9ae66cf05d2436bc035af343e46970ce50
yusuke.sasamori <yusuke.sasamori@rakuten.com>
2020-10-03 22:58:03 +0900

    change

--- src/controller/purchase.go
+++ src/controller/purchase.go
@@ -3,30 +3,38 @@ package controller
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yusukesasamo/go-sample/src/model"
 )
 
 // PurchasePOST purchase item
 func PurchasePOST(c *gin.Context) {
-	// db := model.DBConnect()
+	db := model.DBConnect()
 	authkey := c.PostForm("authkey")
 	itemID, _ := strconv.Atoi(c.PostForm("itemID"))
 	user := FindUserByAuthkey(string(authkey))
 	item := FindByItemID(uint(itemID))
+	now := time.Now()
+
 	fmt.Println(user.Point)
 	fmt.Println(item.Price)
-	// userID := user.ID
-	// userPoint := user.Point
-	// itemID := c.PostForm("itemID")
 
-	// /// updateitem db
-	// now := time.Now()
+	tx, beginError := db.Begin()
+	if beginError != nil {
+		panic(beginError.Error())
+	}
+	_, updateItemErr := db.Exec("UPDATE item SET stock_flg = ?, updated_at=? WHERE id = ?", 0, now, item.ID)
+	if updateItemErr != nil {
+		panic(updateItemErr.Error())
+	}
 
-	// _, err := db.Exec("INSERT INTO item (user_id, name, price, stock_flg, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)", userID, name, price, stockFlg, now, now)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-
-	// fmt.Printf("post sent. name: %s", name)
+	remainPoint := user.Point - item.Price
+	//TODO We will implement logc if remainPoint less than 0
+	_, updateUserErr := db.Exec("UPDATE user SET point = ?, updated_at=? WHERE id = ?", remainPoint, now, user.ID)
+	if updateUserErr != nil {
+		panic(updateUserErr.Error())
+	}
+	tx.Commit()
 }
